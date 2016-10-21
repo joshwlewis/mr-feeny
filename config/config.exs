e15251a6c44e8ecb9ac7584a854c4f9cd1cc0e76
Josh W Lewis <josh.w.lewis@gmail.com>
2016-10-21 00:40:15 -0500

    Add logging info

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+IO.pust("MIX_ENV: #{Mix.env}")
+IO.puts("DATABASE_URL: #{System.get_env("DATABASE_URL")}")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
