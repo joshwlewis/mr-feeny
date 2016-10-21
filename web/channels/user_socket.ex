bcbc522a2883b1fa727e202f64bd2341b950d2ee
Josh W Lewis <josh.w.lewis@gmail.com>
2016-10-20 22:50:23 -0500

    Prepare for heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -4,8 +4,8 @@ defmodule Mrfeeny.UserSocket do
   ## Channels
   # channel "room:*", Mrfeeny.RoomChannel
 
-  ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
